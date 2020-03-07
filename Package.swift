b3878aaeb097effac31b16d2bb70d4ce2fbc604a
Brandon Asuncion <me@brandonasuncion.dev>
2020-03-06 22:53:50 -0800

    Clean up for SPM

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+
+let package = Package(
+    name: "libcmark",
+    products: [
+        .library(
+            name: "libcmark",
+            type: .static,
+            targets: [
+                "libcmark",
+            ]
+        )
+    ],
+    targets: [
+        .target(
+            name: "libcmark",
+            path: ".",
+            exclude: ["src/main.c"]
+            , sources: [
+                "src",
+                "extensions"
+            ]
+            , cSettings: [
+                .headerSearchPath("include"),
+                .headerSearchPath("src"),
+                .headerSearchPath("extensions"),
+            ]
+        )
+    ]
+)
---
+++ prebuild.sh
@@ -0,0 +1,5 @@
+mkdir build
+cd build
+cmake ..
+mv ./src/*.h ../include/
+mv ./extensions/*.h ../include/
